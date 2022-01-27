97fc8756dad56efdca77a696b6e1de5826b703e7
Finnomator <80774024+Finnomator@users.noreply.github.com>
2022-01-27 16:26:43 +0100

    Update Get_current_incidence_all_federal_states.py

--- Get_current_incidence_all_federal_states.py
+++ Get_current_incidence_all_federal_states.py
@@ -1,72 +1,86 @@
-import re
-import requests
-import sys
-import time
-
-class bc:
-    UNDERLINE = '\033[4m'
-    ENDC = '\033[0m'
+import json
+from requests import *
 
 url = "https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html"
-htmltext = requests.get(url).text.replace("\n","")
-
-useful = re.search('7 Tagen</th><th class="right" colspan="1" rowspan="1">7-Tage-<br />(.*)<div class="teaser gbe target">',htmltext).group(1)
-
-Bundeslander = ["Baden-Württem­berg","Bayern","Berlin","Branden­burg","Bremen","Hamburg","Hessen","Meck­lenburg-<br />Vor­pommern","Nieder­sachsen","Nord­rhein-West­falen","Rhein­land-Pfalz","Saarland","Sachsen","Sachsen-Anhalt","Schles­wig-Holstein","Thüringen"]
-Bundnamen = ["Baden-Württemberg","Bayern","Berlin","Brandenburg","Bremen","Hamburg","Hessen","Mecklenburg-Vorpommern","Niedersachsen","Nordrhein-Westfalen","Rheinland-Pfalz","Saarland","Sachsen","Sachsen-Anhalt","Schleswig-Holstein","Thüringen"]
-
-Anzahl = []
-DifferenzzumVortag = []
-Falleindenletzten7tagen = []
-siebenTageInzidenz = []
-Todesfalle = []
-Bundeslanduseful = []
 
 
-for i in range(len(Bundeslander)):
+def scrap_data():
+    response = get(url).text
 
-    Bundeslanduseful.append("")
-    Anzahl.append("")
-    DifferenzzumVortag.append("")
-    Falleindenletzten7tagen.append("")
-    siebenTageInzidenz.append("")
-    Todesfalle.append("")
+    interesting = response.split(
+        "</thead><tbody>")[1].split("</tbody></table>")[0].replace("\n", "")
 
-    if i == 15:
-        Bundeslanduseful[i] = re.search(Bundeslander[i]+'(.*)</strong></td></tr></tbody></table><p><br />',useful).group(1)
-    else:
-        Bundeslanduseful[i] = re.search(Bundeslander[i]+'(.*)'+Bundeslander[i+1],useful).group(1)
+    raw_states = interesting.split('<td colspan="1" rowspan="1">')
 
-    for j in range(Bundeslanduseful[i].count('</td><td class="right" colspan="1" rowspan="1">')):
-        Bundeslanduseful[i] = str(Bundeslanduseful[i]).replace('</td><td class="right" colspan="1" rowspan="1">',"###"+str(j)+"###",1)
+    raw_extracts = []
 
-    
+    for i in range(len(raw_states)):
 
-    Anzahl[i] = re.search("###0###(.*)###1###",Bundeslanduseful[i]).group(1)
-    DifferenzzumVortag[i] = re.search("###1###(.*)###2###",Bundeslanduseful[i]).group(1)
-    Falleindenletzten7tagen[i] = re.search("###2###(.*)###3###",Bundeslanduseful[i]).group(1)
-    siebenTageInzidenz[i] = re.search("###3###(.*)###4###",Bundeslanduseful[i]).group(1)
-    if i == 0 or i == 2 or i == 4 or i == 6 or i == 8 or i == 10 or i == 12 or i == 14 or i == 16:
-        Todesfalle[i] = re.search('###4###(.*)</td></tr><tr><td colspan="1" rowspan="1">',Bundeslanduseful[i]).group(1)
-    else:
-        Todesfalle[i] = re.search('###4###(.*)</td></tr><tr class="even"><td colspan="1" rowspan="1">',Bundeslanduseful[i]).group(1)
+        c = raw_states[i].count(
+            '</td><td class="right" colspan="1" rowspan="1">')
+        raw_extracts.append([])
 
-    print(bc.UNDERLINE+"\nBundesland: "+Bundnamen[i]+":"+bc.ENDC)
-    print("Anzahl: "+Anzahl[i])
-    print("Differez zum Vortag: "+DifferenzzumVortag[i])
-    print("Fälle in den letzten sieben Tagen: "+Falleindenletzten7tagen[i])
-    print("Sieben Tage Inzidenz: "+siebenTageInzidenz[i])
-    print("Todesfälle: "+Todesfalle[i])
+        for j in range(c):
 
-print("\nSource: https://www.rki.de/DE/Content/InfAZ/N/Neuartiges_Coronavirus/Fallzahlen.html\n")
+            raw_extracts[i].append(raw_states[i].split(
+                '</td><td class="right" colspan="1" rowspan="1">')[j])
 
-print("Closing in ",end="")
+        death_cases = raw_states[i].split('rowspan="1">')[
+            raw_states[i].count('rowspan="1">')].split("<")[0]
 
-x = 99
+        raw_extracts[i].append(death_cases)
 
-for m in range(x):
-    sys.stdout.write(str(x-m))
-    sys.stdout.flush()
-    for mm in range(len(str(x-m))):
-        sys.stdout.write('\b')
-    time.sleep(1)
+    raw_extracts.pop(0)
+
+    extracts = {}
+
+    info = ["Anzahl", "Differenz zum Vortag",
+            "Fälle in den lezten 7 Tagen", "7-Tage Inzidenz", "Todesfälle"]
+
+    for i in range(len(raw_extracts)):
+        state = raw_extracts[i][0]
+        state = state.replace("<strong>", "").replace("</strong>", "")
+        state = state.replace("Â­","")
+        state = state.replace("Ã¼","ü")
+        state = state.replace("*","")
+        state = state.replace("-<br />"," ")
+        raw_extracts[i].pop(0)
+
+        temp = {}
+
+        for j in range(len(raw_extracts[i])):
+
+            temp.update({info[j]: raw_extracts[i][j]})
+
+        extracts.update({state: temp})
+
+    return extracts
+
+
+def format_nicly(data):
+
+    res = ""
+
+    for state in data:
+        res += "\n"+state+":\n"
+
+        for case in data[state]:
+
+            res += "    "
+            res += case+": "+data[state][case]+"\n"
+
+    return res
+
+
+def get_nicly_formated():
+
+    return format_nicly(scrap_data())
+
+if __name__ == "__main__":
+
+    data = scrap_data()
+
+    print(format_nicly(data))
+
+    print()
+    print("Source: "+url)
